703f4bab07ec789b3e3c58702cee64adadabf21c
Su Su <s@warmsea.net>
2014-04-10 17:59:44 +0800

    WarmseaJS now supports Node.js.

--- src/outro.js
+++ src/outro.js
@@ -1,4 +1,6 @@
-  if ( typeof define === 'function' && define.amd) {
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = w;
+  } else if ( typeof define === 'function' && define.amd) {
     define(function() {
       return w;
     });
